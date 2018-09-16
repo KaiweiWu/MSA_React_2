0eb8df938bd86c64242843aff6edace2ccb2a917
Kaiwei Wu <kaiwei-wu@hotmail.com>
2018-09-16 19:35:30 +1200

    Api working

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import Background from 'D:/MSA_React_2/my-app/src/clouds.png';
 import * as React from 'react';
 import './App.css';
 
+interface IState {
+  results: any,
+}
+
 const sectionStyle = {
   backgroundImage: `url(${Background})`,
   height: "800px",
@@ -23,7 +27,34 @@ const styles = ({
   },
 });
 
-class App extends React.Component<{}> {
+class App extends React.Component<{}, IState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      results: "",
+    };
+    this.onClickAction = this.onClickAction.bind(this);
+  }
+
+  public onClickAction() {
+      const inputValue = (document.getElementById("helperText") as HTMLInputElement).value;
+      const url = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&appid=f5f6506f9423a23fe54359e30b41b253";
+
+      fetch(url, {
+        headers: {
+            "Content-Type": "text/plain",
+          },
+        method: "GET",
+      })
+    .then(response => {
+      if (response.ok) {
+        response.json().then((data:any) => this.setState({results: "Current weather: " + data.weather[0].description}))
+      } else {
+        this.setState({results: response.statusText})
+      }
+    })
+  }
 
   public render() {
     return (
@@ -40,10 +71,12 @@ class App extends React.Component<{}> {
           margin="normal"
         />
         </h1>
-        <Button variant="contained" className={"button"} onClick={onClickAction()}>
+        <Button variant="contained" className={"button"} onClick={this.onClickAction}>
           Find
         </Button>
-        <p id = "demo">A function is triggered when the button is clicked. The function outputs some text in a p element with id="demo".</p>
+        <div className="weatherInfo">
+          <p>{this.state.results}</p>
+        </div>
       </div>
     </div>
     </section>
@@ -51,9 +84,7 @@ class App extends React.Component<{}> {
   }
 }
 
-function onClickAction(x, y) {
-    return x + y;
-}
+
 
 
 export default withStyles(styles)(App);
